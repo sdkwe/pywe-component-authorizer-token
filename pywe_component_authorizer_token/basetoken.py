fdb8b0fae8576b258203a068bca005a61e549295
Brightcells <brightcells@gmail.com>
2018-05-02 04:46:18 +0800

    :sparkles: Add `token_fetched_func=None` and `auth_token_fetched_func=None`

--- pywe_component_authorizer_token/basetoken.py
+++ pywe_component_authorizer_token/basetoken.py
@@ -8,7 +8,7 @@ from pywe_storage import MemoryStorage, ShoveStorage
 
 
 class BaseComponentAuthorizerToken(BaseWechat):
-    def __init__(self, component_appid=None, component_secret=None, auth_code=None, auth_access_token=None, auth_refresh_token=None, storage=None):
+    def __init__(self, component_appid=None, component_secret=None, auth_code=None, auth_access_token=None, auth_refresh_token=None, storage=None, token_fetched_func=None, auth_token_fetched_func=None):
         super(BaseComponentAuthorizerToken, self).__init__()
         self.component_appid = component_appid
         self.component_secret = component_secret
@@ -16,17 +16,22 @@ class BaseComponentAuthorizerToken(BaseWechat):
         self.auth_access_token = auth_access_token
         self.auth_refresh_token = auth_refresh_token
         self.storage = storage
+        self.token_fetched_func = token_fetched_func
+        self.auth_token_fetched_func = auth_token_fetched_func
 
     def component_authorizer_access_info_key(self, authorizer_appid=None):
         return '{0}:{1}:component:authorizer:access:info'.format(self.component_appid, authorizer_appid)
 
-    def update_params(self, component_appid=None, component_secret=None, auth_code=None, auth_access_token=None, auth_refresh_token=None, storage=None):
+    def update_params(self, component_appid=None, component_secret=None, auth_code=None, auth_access_token=None, auth_refresh_token=None, storage=None, token_fetched_func=None, auth_token_fetched_func=None):
+        self.storage = storage or self.storage
+
+        if self.storage is None or isinstance(self.storage, (MemoryStorage, ShoveStorage)):
+            raise WeChatException('Can not use memory storage, Use RedisStorage or MemcachedStorage instead.')
+
         self.component_appid = component_appid or self.component_appid
         self.component_secret = component_secret or self.component_secret
         self.auth_code = auth_code or self.auth_code
         self.auth_access_token = auth_access_token or self.auth_access_token
         self.auth_refresh_token = auth_refresh_token or self.auth_refresh_token
-        self.storage = storage or self.storage
-
-        if self.storage is None or isinstance(self.storage, (MemoryStorage, ShoveStorage)):
-            raise WeChatException('Can not use memory storage, Use RedisStorage or MemcachedStorage instead.')
+        self.token_fetched_func = token_fetched_func or self.token_fetched_func
+        self.auth_token_fetched_func = auth_token_fetched_func or self.auth_token_fetched_func
--- pywe_component_authorizer_token/token.py
+++ pywe_component_authorizer_token/token.py
@@ -9,8 +9,8 @@ from .basetoken import BaseComponentAuthorizerToken
 
 
 class ComponentAuthorizerToken(BaseComponentAuthorizerToken):
-    def __init__(self, component_appid=None, component_secret=None, auth_code=None, storage=None):
-        super(ComponentAuthorizerToken, self).__init__(component_appid=component_appid, component_secret=component_secret, auth_code=auth_code, storage=storage)
+    def __init__(self, component_appid=None, component_secret=None, auth_code=None, storage=None, token_fetched_func=None, auth_token_fetched_func=None):
+        super(ComponentAuthorizerToken, self).__init__(component_appid=component_appid, component_secret=component_secret, auth_code=auth_code, storage=storage, token_fetched_func=token_fetched_func, auth_token_fetched_func=auth_token_fetched_func)
         # 授权流程技术说明, Refer: https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=open1453779503&token=&lang=zh_CN
         # 4、使用授权码换取公众号或小程序的接口调用凭据和授权信息
         # 该API用于使用授权码换取授权公众号或小程序的授权信息，并换取authorizer_access_token和authorizer_refresh_token。 授权码的获取，需要在用户在第三方平台授权页中完成授权流程后，在回调URI中通过URL参数提供给第三方平台方。请注意，由于现在公众号或小程序可以自定义选择部分权限授权给第三方平台，因此第三方平台开发者需要通过该接口来获取公众号或小程序具体授权了哪些权限，而不是简单地认为自己声明的权限就是公众号或小程序授权的权限。
@@ -22,11 +22,11 @@ class ComponentAuthorizerToken(BaseComponentAuthorizerToken):
     def __about_to_expires(self, expires_at):
         return expires_at and expires_at - int(time.time()) < 60
 
-    def __fetch_authorizer_access_token(self, component_appid=None, component_secret=None, auth_code=None, storage=None):
+    def __fetch_authorizer_access_token(self, component_appid=None, component_secret=None, auth_code=None, storage=None, token_fetched_func=None, auth_token_fetched_func=None):
         # Update Params
-        self.update_params(component_appid=component_appid, component_secret=component_secret, auth_code=auth_code, storage=storage)
+        self.update_params(component_appid=component_appid, component_secret=component_secret, auth_code=auth_code, storage=storage, token_fetched_func=token_fetched_func, auth_token_fetched_func=auth_token_fetched_func)
         # Component Authorizer Token Request
-        token = component_access_token(appid=self.component_appid, secret=self.component_secret, storage=self.storage)
+        token = component_access_token(appid=self.component_appid, secret=self.component_secret, storage=self.storage, token_fetched_func=self.token_fetched_func)
         component_authorizer_access_info = self.post(self.WECHAT_FETCH_AUTHORIZER_TOKEN.format(component_access_token=token), data={
             'component_appid': self.component_appid,
             'authorization_code': self.auth_code,
@@ -37,15 +37,19 @@ class ComponentAuthorizerToken(BaseComponentAuthorizerToken):
         # Set Authorizer Access Info into Storage
         expires_in = component_authorizer_access_info.get('expires_in')
         component_authorizer_access_info['expires_at'] = int(time.time()) + expires_in
-        self.storage.set(self.component_authorizer_access_info_key(authorizer_appid=component_authorizer_access_info.get('authorizer_appid', '')), component_authorizer_access_info)
+        authorizer_appid = component_authorizer_access_info.get('authorizer_appid', '')
+        self.storage.set(self.component_authorizer_access_info_key(authorizer_appid=authorizer_appid), component_authorizer_access_info)
+        # If auth_token_fetched_func, Call it with `appid`, `secret`, `access_info`
+        if auth_token_fetched_func:
+            auth_token_fetched_func(self.component_appid, self.component_secret, authorizer_appid, component_authorizer_access_info)
         # Return Access Token
         return component_authorizer_access_info.get('authorizer_access_token')
 
-    def __refresh_authorizer_access_token(self, component_appid=None, component_secret=None, authorizer_appid=None, storage=None):
+    def __refresh_authorizer_access_token(self, component_appid=None, component_secret=None, authorizer_appid=None, storage=None, token_fetched_func=None, auth_token_fetched_func=None):
         # Update Params
-        self.update_params(component_appid=component_appid, component_secret=component_secret, storage=storage)
+        self.update_params(component_appid=component_appid, component_secret=component_secret, storage=storage, token_fetched_func=token_fetched_func)
         # Component Authorizer Token Refresh Request
-        token = component_access_token(appid=self.component_appid, secret=self.component_secret, storage=self.storage)
+        token = component_access_token(appid=self.component_appid, secret=self.component_secret, storage=self.storage, token_fetched_func=self.token_fetched_func)
         component_authorizer_access_info = self.post(self.WECHAT_REFRESH_AUTHORIZER_TOKEN.format(component_access_token=token), data={
             'component_appid': self.component_appid,
             'authorizer_appid': authorizer_appid,
@@ -58,28 +62,31 @@ class ComponentAuthorizerToken(BaseComponentAuthorizerToken):
         expires_in = component_authorizer_access_info.get('expires_in')
         component_authorizer_access_info['expires_at'] = int(time.time()) + expires_in
         self.storage.set(self.component_authorizer_access_info_key(authorizer_appid=authorizer_appid), component_authorizer_access_info)
+        # If token_fetched_func, Call it with `appid`, `secret`, `access_info`
+        if auth_token_fetched_func:
+            auth_token_fetched_func(self.component_appid, self.component_secret, authorizer_appid, component_authorizer_access_info)
         # Return Access Token
         return component_authorizer_access_info.get('authorizer_access_token')
 
-    def get_authorizer_access_token(self, component_appid=None, component_secret=None, authorizer_appid=None, storage=None):
+    def get_authorizer_access_token(self, component_appid=None, component_secret=None, authorizer_appid=None, storage=None, token_fetched_func=None, auth_token_fetched_func=None):
         # Update Params
-        self.update_params(component_appid=component_appid, component_secret=component_secret, storage=storage)
+        self.update_params(component_appid=component_appid, component_secret=component_secret, storage=storage, token_fetched_func=token_fetched_func, auth_token_fetched_func=auth_token_fetched_func)
         # Fetch component_authorizer_access_info
         component_authorizer_access_info = self.storage.get(self.component_authorizer_access_info_key(authorizer_appid=authorizer_appid))
         if component_authorizer_access_info:
             authorizer_access_token = component_authorizer_access_info.get('authorizer_access_token')
             if authorizer_access_token and not self.__about_to_expires(component_authorizer_access_info.get('expires_at')):
                 return authorizer_access_token
-        return self.__refresh_authorizer_access_token(component_appid, component_secret, authorizer_appid, storage)
+        return self.__refresh_authorizer_access_token(self.component_appid, self.component_secret, authorizer_appid, self.storage, token_fetched_func=self.token_fetched_func, auth_token_fetched_func=self.auth_token_fetched_func)
 
-    def initial_authorizer_access_token(self, component_appid=None, component_secret=None, auth_code=None, storage=None):
-        return self.__fetch_authorizer_access_token(component_appid, component_secret, auth_code, storage)
+    def initial_authorizer_access_token(self, component_appid=None, component_secret=None, auth_code=None, storage=None, token_fetched_func=None, auth_token_fetched_func=None):
+        return self.__fetch_authorizer_access_token(component_appid, component_secret, auth_code, storage, token_fetched_func=token_fetched_func, auth_token_fetched_func=auth_token_fetched_func)
 
-    def refresh_authorizer_access_token(self, component_appid=None, component_secret=None, authorizer_appid=None, storage=None):
-        return self.__refresh_authorizer_access_token(component_appid, component_secret, authorizer_appid, storage)
+    def refresh_authorizer_access_token(self, component_appid=None, component_secret=None, authorizer_appid=None, storage=None, token_fetched_func=None, auth_token_fetched_func=None):
+        return self.__refresh_authorizer_access_token(component_appid, component_secret, authorizer_appid, storage, token_fetched_func=token_fetched_func, auth_token_fetched_func=auth_token_fetched_func)
 
-    def final_authorizer_access_token(self, cls=None, component_appid=None, component_secret=None, authorizer_access_token=None, authorizer_appid=None, storage=None):
-        return authorizer_access_token or self.get_authorizer_access_token(component_appid or cls.component_appid, component_secret or cls.component_secret, authorizer_appid, storage=storage or cls.storage)
+    def final_authorizer_access_token(self, cls=None, component_appid=None, component_secret=None, authorizer_access_token=None, authorizer_appid=None, storage=None, token_fetched_func=None, auth_token_fetched_func=None):
+        return authorizer_access_token or self.get_authorizer_access_token(component_appid or cls.component_appid, component_secret or cls.component_secret, authorizer_appid, storage=storage or cls.storage, token_fetched_func=token_fetched_func or cls.token_fetched_func, auth_token_fetched_func=auth_token_fetched_func)
 
 
 token = ComponentAuthorizerToken()
